70b3163f98995509b7329afb158c1ab0d5bc025c
唐道海 <tangdaohai@outlook.com>
2020-01-24 23:32:07 +0800

    feat(views/user_detail): 用户详情页UI调整 移除 Stack 布局

--- lib/views/users/detail.dart
+++ lib/views/users/detail.dart
@@ -11,72 +11,49 @@ class _State extends State<UserDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Stack(
-        // overflow: Overflow.visible,
-        fit: StackFit.expand,
+      body: ListView(
         children: <Widget>[
-          Positioned(
-            top: 0,
-            child: Image(
-              image: AssetImage('images/126244.jpg'),
-              width: ScreenUtil().setWidth(375),
+          // 头像图片
+          Image(
+            image: AssetImage('images/126244.jpg'),
+            width: ScreenUtil().setWidth(375),
+            height: ScreenUtil().setHeight(280),
+            fit: BoxFit.fitWidth,
+          ),
+          // 备注与昵称 以及星标
+          ListTile(
+            leading: Icon(Icons.person, size: 40,),
+            title: Text('悠扬小Q'),
+            subtitle: Text('昵称：悠扬小Q'),
+            trailing: GestureDetector(
+              onTap: () {
+                print('星标');
+              },
+              child: Icon(Icons.star),
             ),
           ),
-          Positioned.fill(
-            top: ScreenUtil().setHeight(280),
-            child: ListView(
-              children: <Widget>[
-                Card(
-                  // elevation: 0,
-                  shape: RoundedRectangleBorder(),
-                  child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        title: Text('悠扬小Q'),
-                        subtitle: Text('昵称：悠扬小Q'),
-                      ),
-                      Divider(height: 1,),
-                      ListTile(
-                        title: Text('EA888'),
-                        subtitle: Text('ID'),
-                      ),
-                      Divider(height: 1,),
-                    ],
-                  ),
-                ),
-                Card(
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(),
-                  child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        title: Text('程序员'),
-                        subtitle: Text('标签'),
-                      ),
-                      Divider(height: 1,),
-                      ListTile(
-                        title: Text('悠扬小Q'),
-                        subtitle: Text('昵称：悠扬小Q'),
-                      ),
-                      Divider(height: 1,),
-                      ListTile(
-                        title: Text('悠扬小Q'),
-                        subtitle: Text('昵称：悠扬小Q'),
-                      ),
-                      Divider(height: 1,),
-                      ListTile(
-                        title: Text('悠扬小Q'),
-                        subtitle: Text('昵称：悠扬小Q'),
-                      ),
-                      Divider(height: 1,),
-                    ],
-                  ),
-                )
-              ],
-            ),
+          // 个人介绍（个性签名）
+          Padding(
+            padding: EdgeInsets.fromLTRB(16, 0, 16, 24),
+            child: Text('Flutter appyes', style: Theme.of(context).textTheme.caption,),
           ),
+          Divider(height: 1,),
+          // id
+          ListTile(
+            leading: Icon(Icons.drag_handle, size: 40),
+            title: Text('EA888'),
+            subtitle: Text('ID'),
+          ),
+          Divider(height: 1,),
+          // 对这个人的标签
+          ListTile(
+            leading: Icon(Icons.bookmark, size: 40,),
+            title: Text('程序员'),
+            subtitle: Text('标签'),
+          ),
+          Divider(height: 1,),
         ],
-      ),
+      )
     );
   }
 }
